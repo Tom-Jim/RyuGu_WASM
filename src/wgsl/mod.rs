242b005e0da47abf4e76f9db9505e304e00ff477
jimtom <macos@jimtoms-MacBook-Air.local>
2026-09-01 13:51:32 +0800

    improve equity of comparison

--- src/wgsl/mod.rs
+++ src/wgsl/mod.rs
@@ -14,7 +14,6 @@ pub(crate) enum EmbeddedShader {
     Gravity,
     Werner,
     Eq106,
-    Eq106Nufft,
     Mmfft,
     Fmm,
     Normals,
@@ -30,7 +29,6 @@ pub(crate) fn load(server: &AssetServer, shader: EmbeddedShader) -> Handle<Shade
         EmbeddedShader::Gravity => load_embedded_asset!(server, "gravity.wgsl"),
         EmbeddedShader::Werner => load_embedded_asset!(server, "werner_gravity.wgsl"),
         EmbeddedShader::Eq106 => load_embedded_asset!(server, "eq106_complex.wgsl"),
-        EmbeddedShader::Eq106Nufft => load_embedded_asset!(server, "eq106_nufft.wgsl"),
         EmbeddedShader::Mmfft => load_embedded_asset!(server, "mmfft_compressed.wgsl"),
         EmbeddedShader::Fmm => load_embedded_asset!(server, "fmm_gravity.wgsl"),
         EmbeddedShader::Normals => load_embedded_asset!(server, "normals.wgsl"),
@@ -51,7 +49,6 @@ impl Plugin for WgslPlugin {
         embedded_asset!(app, "gravity.wgsl");
         embedded_asset!(app, "werner_gravity.wgsl");
         embedded_asset!(app, "eq106_complex.wgsl");
-        embedded_asset!(app, "eq106_nufft.wgsl");
         embedded_asset!(app, "mmfft_compressed.wgsl");
         embedded_asset!(app, "fmm_gravity.wgsl");
         embedded_asset!(app, "normals.wgsl");
